2c6179b46d5c223da927f7ca050baae859b7c413
Maxim <polyakovmaxim05@gmail.com>
2022-12-16 10:31:13 -0500

    commit tree 408ed2d9cea9401a0fc84ae4554b4159fcfa3976

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,23 @@
-# your code here
+class Stack
+    def initialize limit = nil
+        @stack = []
+        @limit = limit
+    end
+
+    def full?
+        @limit && stack.size = @limit
+    end 
+
+    def push(item)
+      raise "Error" if full?
+      @stack.push(item)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+end
